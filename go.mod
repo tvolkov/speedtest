072eb9cdb8fc7c5de8fccf22d7947d99c71cf14d
Timofey Volkov <timofey.volkov@tiger.trade>
2022-01-16 19:50:49 +0100

    handle and log errors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tvolkov/speedtest
 go 1.17
 
 require (
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
 	github.com/showwin/speedtest-go v1.1.4 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 )
--- speedtestnet/speedtestnet.go
+++ speedtestnet/speedtestnet.go
@@ -1,29 +1,65 @@
 package speedtestnet
 
 import (
-	"fmt"
+	"errors"
+
+	"github.com/op/go-logging"
 
 	"github.com/showwin/speedtest-go/speedtest"
 )
 
+var log = logging.MustGetLogger("speedtest.net")
+
 func TestSpeed() (float64, float64, error) {
-	fmt.Println("Hello from speedtest.net gauge")
-	user, _ := speedtest.FetchUserInfo()
-	serverList, _ := speedtest.FetchServerList(user)
-	targets, _ := serverList.FindServer([]int{})
+	log.Info("Testing speed using speedtest.net")
+	user, err := speedtest.FetchUserInfo()
+
+	if err != nil {
+		log.Warningf("Error fetching user info: %s", err)
+		return 0, 0, errors.New("Error fetching user info")
+	}
+
+	serverList, err := speedtest.FetchServerList(user)
+
+	if err != nil {
+		log.Warningf("error fetching server list: %s", err)
+		return 0, 0, errors.New("error fetching server list")
+	}
+
+	targets, err := serverList.FindServer([]int{})
+
+	if err != nil {
+		log.Warningf("error finding server: %s")
+		return 0, 0, errors.New("error finding server")
+	}
 
 	downloadSpeedSum := 0.0
 	uploadSpeedSum := 0.0
 	n := targets.Len()
 
 	for _, s := range targets {
-		s.PingTest()
-		s.DownloadTest(false)
-		s.UploadTest(false)
+		err = s.PingTest()
+		if err != nil {
+			log.Warningf("unable to test ping")
+		}
 
-		fmt.Printf("Latency: %s, Download: %f, Upload: %f\n", s.Latency, s.DLSpeed, s.ULSpeed)
+		err = s.DownloadTest(false)
+		if err != nil {
+			log.Warningf("unable to test download speed")
+		}
+
+		err = s.UploadTest(false)
+		if err != nil {
+			log.Warningf("unable to test upload speed")
+		}
+
+		log.Infof("Latency: %s, Download: %f, Upload: %f\n", s.Latency, s.DLSpeed, s.ULSpeed)
 		downloadSpeedSum += s.DLSpeed
 		uploadSpeedSum += s.ULSpeed
+	}
+
+	if downloadSpeedSum == 0 || uploadSpeedSum == 0 {
+		log.Warningf("could not test connection speed, try again")
 	}
 
 	return downloadSpeedSum / float64(n), uploadSpeedSum / float64(n), nil
