3aa22d5394a13ed265ec3e1654ad934ec613c11e
Timofey Volkov <timofey.volkov@tiger.trade>
2022-01-16 22:55:22 +0100

    benchmark test

--- speedtest_test.go
+++ speedtest_test.go
@@ -8,10 +8,35 @@ import (
 	"github.com/tvolkov/speedtest"
 )
 
-func Test(t *testing.T) {
+func TestFastCom(t *testing.T) {
 
 	s, e := speedtest.Speedtest("fastcom")
 
 	fmt.Println(s)
 	fmt.Println(e)
+}
+
+func TestSpeedtestNet(t *testing.T) {
+	s, e := speedtest.Speedtest("speedtest")
+
+	fmt.Println(s)
+	fmt.Println(e)
+}
+
+func BenchmarkFastCom(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s, e := speedtest.Speedtest("fastcom")
+
+		fmt.Println(s)
+		fmt.Println(e)
+	}
+}
+
+func BenchmarkSpeedtestNet(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		s, e := speedtest.Speedtest("speedtest")
+
+		fmt.Println(s)
+		fmt.Println(e)
+	}
 }
